5c1cb9b45d04b517417fb876841293db1e677695
Monique <moniqueking57@gmail.com>
2021-05-01 12:25:17 +0100

    ....

---
+++ data.js
@@ -0,0 +1,28 @@
+const messages = [
+  {
+    id: '0',
+    from: 'Bart',
+    text: 'Welcome to CYF chat system!',
+    timeStamp: Date(),
+  },
+  {
+    id: '1',
+    from: 'Meedaxa',
+    text: 'Welcome!',
+    timeStamp: Date(),
+  },
+  {
+    id: '2',
+    from: 'Meea',
+    text: 'Welo!',
+    timeStamp: Date(),
+  },
+  {
+    id: '3',
+    from: 'change',
+    text: 'hi!',
+    timeStamp: Date(),
+  },
+];
+
+module.exports = messages;
